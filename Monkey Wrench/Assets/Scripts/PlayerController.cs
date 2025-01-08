23cea331d3e9b4c88454d90b8ae8cae558a8f857
C342 <mbuzurin2024@elam.co.uk>
2025-01-08 12:44:41 +0000

    Upd

--- Monkey Wrench/Assets/Scripts/PlayerController.cs
+++ Monkey Wrench/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float JumpForce = 5f;
+    float xRotation = 0f;
+
+    [SerializeField] AudioSource jumpSound;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        rb.velocity = new Vector3(horizontalInput * 5f, rb.velocity.y, verticalInput * 5f);
+        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+        rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+        jumpSound.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
+            Jump();
+        }
     }
 }
