9b5d1f4c16ccad45766da5fbf981075e9f0dc8fb
C342 <mbuzurin2024@elam.co.uk>
2025-01-09 10:43:09 +0000

    Upd2

--- Monkey Wrench/Assets/Scripts/PlayerController.cs
+++ Monkey Wrench/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,6 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float JumpForce = 5f;
-    float xRotation = 0f;
-
-    [SerializeField] AudioSource jumpSound;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -23,17 +20,9 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
         rb.velocity = new Vector3(horizontalInput * 5f, rb.velocity.y, verticalInput * 5f);
         Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
         rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
-    }
-
-    void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
-        jumpSound.Play();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -41,7 +30,6 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy Head"))
         {
             Destroy(collision.transform.parent.gameObject);
-            Jump();
         }
     }
 }
