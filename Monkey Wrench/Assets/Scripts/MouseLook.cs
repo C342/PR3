038ebd8d77e1227fb64adc1610db9b0a75a48e96
C342 <mbuzurin2024@elam.co.uk>
2025-01-10 15:25:48 +0000

    CameraFix

---
+++ Monkey Wrench/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Vector3 MouseScreenToCameraSpace = new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.y);
+        Vector3 PlayerScreenToCameraSpace = new(Camera.main.WorldToScreenPoint(transform.position).x, 0f, Camera.main.WorldToScreenPoint(transform.position);
+        //Debug.Log(Input.mousePosition);
+        Vector3 PlayerToMouse = MouseScreenToCameraSpace - PlayerScreenToCameraSpace;
+        Debug.DrawLine(transform.position, PlayerToMouse);
+        transform.LookAt(PlayerToMouse);
+    }
+}
