055f032362199253de3d0ce28a1544da88575bae
C342 <mbuzurin2024@elam.co.uk>
2025-01-16 14:46:47 +0000

    pickup + UI

---
+++ Monkey Wrench/Assets/Scripts/WrenchInteract.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UIElements;
+
+public class WrenchInteract : MonoBehaviour
+{
+    public GameObject WrenchPickedUp;
+    public GameObject WrenchIcon;
+
+    void Start()
+    {
+        WrenchPickedUp.SetActive(false);
+        WrenchIcon.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                WrenchIcon.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                this.gameObject.SetActive(false);
+
+                WrenchPickedUp.SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == null)
+        {
+            WrenchIcon.SetActive(false);
+        }
+    }
+}
