2eaa58241690c0ba0a9d6e4b457e3fd9bfb19fb0
C342 <mbuzurin2024@elam.co.uk>
2025-01-16 12:45:32 +0000

    h

--- Monkey Wrench/Assets/Scripts/WrenchThrow.cs
+++ Monkey Wrench/Assets/Scripts/WrenchThrow.cs
@@ -1,11 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WrenchThrow : MonoBehaviour
 {
-    private void Start()
-    {
-    
-    }
+
 }
