5dbc37b9f8abcf0fad8449f3304c508eeb8f446a
C342 <mbuzurin2024@elam.co.uk>
2025-01-16 10:19:00 +0000

    h

---
+++ Monkey Wrench/Assets/Scripts/WrenchThrow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrenchThrow : MonoBehaviour
+{
+    Vector3 throwVector;
+    Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+    void CalculateThrowVector;
+    {
+        
+    }
+}
