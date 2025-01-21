d95a24bb31883965c929afc6613d7365d3f63040
C342 <mbuzurin2024@elam.co.uk>
2025-01-21 14:39:32 +0000

    m

--- Monkey Wrench/Assets/Scripts/WrenchThrow.cs
+++ Monkey Wrench/Assets/Scripts/WrenchThrow.cs
@@ -6,5 +6,15 @@ using UnityEngine.Rendering;
 
 public class WrenchThrow : MonoBehaviour
 {
+    public bool Pickupable, pickedup;
+    public Rigidbody objectRigidbody;
+    public float throwAmount;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
 }
